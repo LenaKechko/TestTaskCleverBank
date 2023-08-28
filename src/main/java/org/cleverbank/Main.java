9efa1f3c07c7e5a20d663c8b4e6b424be8e4a375
Elena Kechko <ekechko@gmail.com>
2023-08-28 15:31:20 +0300

    Create project

---
+++ src/main/java/org/cleverbank/Main.java
@@ -0,0 +1,52 @@
+package org.cleverbank;
+
+import org.cleverbank.DAO.UserDAO;
+import org.cleverbank.Menu.MainMenu;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в приложение Clever-Bank.\nВыберите операцию:");
+//        MainMenu.start();
+//        System.out.println("Testing connection");
+//        UserDAO userDAO = new UserDAO();
+//        System.out.println(userDAO.findAll());
+//        try {
+//            Class.forName("org.postgresql.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//        System.out.println("Connection successful");
+//        Connection connection = null;
+//        try {
+//            connection = ConnectorDB.getConnection();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        if (connection != null) {
+//            System.out.println("Connection with database");
+//            try {
+//                Statement statement = connection.createStatement();
+//                ResultSet rs = statement.executeQuery("select * from banks");
+//                while (rs.next()) {
+//                    System.out.println(rs.getInt("id"));
+//                    System.out.println(rs.getString("name"));
+//
+//                }
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//
+//        } else {
+//            System.out.println("Fail connection");
+//        }
+
+    }
+}
